a46977deca7e6713bb2a942430ba367789d6e061
Emanuel Coen <emanuelcoen@gmail.com>
2018-07-25 10:45:41 +0200

    finished lists

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,37 @@
 class ProfilesController < ApplicationController
+
+  def index
+    search = params[:term].present? ? params[:term] : nil
+    @users = if search
+    User.search(search)
+    else
+      User.all
+    end
+  end
+
+  def autocomplete
+    render json: User.search(params[:query], {
+      fields: ["first_name", "last_name", "username"],
+      match: :word_start,
+      limit: 10,
+      load: false,
+      misspellings: {below: 5}
+    }).map(&:username)
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def profile
     @my_lists = current_user.lists
+  end
+
+  private
+  def set_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username)
   end
 end
