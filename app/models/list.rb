abc32379d3fd128d7f4bf7d8d9460090e83f87d6
Emanuel Coen <emanuelcoen@gmail.com>
2018-08-15 18:55:13 +0200

    now possible to add items without description

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,7 @@ class List < ApplicationRecord
   include ::PublicActivity::Common
   belongs_to :user
   has_many :items, dependent: :destroy
-  accepts_nested_attributes_for :items, allow_destroy: true, reject_if: proc { |att| att['description'].blank?}
+  accepts_nested_attributes_for :items, allow_destroy: true
 end
 
+# , reject_if: proc { |att| att['description'].blank?}
