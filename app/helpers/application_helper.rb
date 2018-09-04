4528017c008dd95a6890c81d042b835269ed1e2a
Emanuel Coen <emanuelcoen@gmail.com>
2018-09-04 17:18:14 +0200

    fixed some bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
   def liked_list(list)
-    liked = current_user.liked?(list) ? 'liked' : ''
+    if current_user.username == "guestuser"
+      liked = ''
+    else
+      liked = current_user.liked?(list) ? 'liked' : ''
+    end
     content_tag(:i, '', class: "fa fa-heart #{liked}")
   end
-
 end
