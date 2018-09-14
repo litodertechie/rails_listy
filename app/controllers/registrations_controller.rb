4bc37f3e75aa90fed40bf3b5e649c7d281765286
Emanuel Coen <emanuelcoen@gmail.com>
2018-09-14 19:10:46 +0200

    back to old

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -67,7 +67,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   # DELETE /resource
   def destroy
-    pry
     resource.destroy
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message! :notice, :destroyed
